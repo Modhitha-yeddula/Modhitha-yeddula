3a4c00e730133053c47b1e0fa483afefb6949590
Modhitha-yeddula <151664477+Modhitha-yeddula@users.noreply.github.com>
2024-08-01 12:20:35 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# This is a simple Python program that prints "Hello, World!" to the console.
+
+def main():
+    # Print "Hello, World!" to the console
+    print("Hello, World!")
+
+# Call the main function to execute the program
+if __name__ == "__main__":
+    main()
