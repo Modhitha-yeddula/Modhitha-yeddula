e1cae725e013af86fbdc804e910ed6e4fef4a057
Modhitha-yeddula <151664477+Modhitha-yeddula@users.noreply.github.com>
2024-08-01 12:19:27 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  // Include the standard input/output header file
+
+// The main function is the entry point of every C program
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Hello, World!\n");
+    
+    // Return 0 to indicate that the program executed successfully
+    return 0;
+}
